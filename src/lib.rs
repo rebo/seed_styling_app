aab08572d0e2dc2618297bb94d2d438621fea0d3
Benjamin Mapp <rebo@Benjamins-MacBook-Pro.local>
2020-04-08 05:17:13 +0100

    re-orded base example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
+#![feature(track_caller)]
 use seed::{*, prelude::*};
+use seed_hooks::*;
 
+
+//  Model, Msg, Update, init(), and start()
+//  ---------------------------------------
 struct Model {}
 
 
@@ -10,12 +15,6 @@ enum Msg {
 fn update(_msg: Msg, _model: &mut Model, _: &mut impl Orders<Msg>) {
 }
 
-
-fn view(_model: &Model) -> impl IntoNodes<Msg> {
-    div![]
-}
-
-
 fn init(_url: Url, _orders: &mut impl Orders<Msg>) -> Model {
     Model {}
 }
@@ -25,4 +24,18 @@ fn init(_url: Url, _orders: &mut impl Orders<Msg>) -> Model {
 pub fn start() {
     App::start("app", init, update, view);
 }
+
+//  View code here!
+//  ---------------
+#[topo::nested]
+fn view(_model: &Model) -> impl IntoNodes<Msg> {
+    let counter = use_state(||0);
+    button![
+     "Clicked ", counter, " times",
+     counter.mouse_ev(Ev::Click, |v,_| *v += 1)
+    ]
+}
+
+
+
 
