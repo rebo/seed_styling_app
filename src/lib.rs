982343ff9d3767ac27f019b7ea9f535727b4bb05
Benjamin Mapp <rebo@Benjamins-MacBook-Pro.local>
2020-04-08 05:03:00 +0100

    basic empty app

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use seed::{*, prelude::*};
+
+struct Model {}
+
+
+#[derive(Clone)]
+enum Msg {
+}
+
+fn update(_msg: Msg, _model: &mut Model, _: &mut impl Orders<Msg>) {
+}
+
+
+fn view(_model: &Model) -> impl IntoNodes<Msg> {
+    div![]
+}
+
+
+fn init(_url: Url, _orders: &mut impl Orders<Msg>) -> Model {
+    Model {}
+}
+
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    App::start("app", init, update, view);
+}
